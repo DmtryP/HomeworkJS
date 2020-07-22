84a9883554c7993ff1f7998b9e3a18874725fbde
DmtryP <listochekcs@gmail.com>
2020-07-22 18:47:29 +0300

    commit

---
+++ ToDoList (GreatDay) (Final)/event.js
@@ -0,0 +1,139 @@
+var input = document.querySelector("input[type = 'text']");
+var inputtime = document.querySelector("input[type = 'time']");
+var select = document.getElementById("priority");
+var ul = document.querySelector("ul");
+var body = document.querySelector("body");
+var h1 = document.querySelector("h1");
+var button = document.querySelectorAll("button");
+var container = document.querySelector("div");
+var spans = document.getElementsByTagName("span");
+var plus_close = document.querySelector("#plus_close");
+var saveBtn = document.querySelector(".save");
+var clearBtn = document.querySelector(".clear");
+var tipsBtn = document.querySelector(".tipBtn");
+var closeBtn = document.querySelector(".closeBtn");
+var overlay = document.getElementById("overlay");
+var weather = document.getElementById("weather_container");
+
+export function deleteTodo(){
+    for(let span of spans){
+      span.addEventListener ("click",function (){
+        span.parentElement.remove();
+        event.stopPropagation();
+      });
+    }
+  }
+
+export function inputListener(){
+    input.addEventListener("keypress",function(keyPressed){
+      if(keyPressed.which === 13){
+        if(ul.offsetHeight <= (body.offsetHeight-320)){
+        var li = document.createElement("li");
+  
+        var texttag = document.createElement("spantext");
+        texttag.setAttribute('id', 'text_container');
+  
+        var spanPriority = document.createElement("spanother");
+        spanPriority.setAttribute('id', 'indicator_container');
+        var iconPriority = document.createElement("img");
+  
+        var spanDelete = document.createElement("span");
+        spanDelete.setAttribute('id', 'basket');
+        var iconDelete = document.createElement("img");
+  
+        var Deadline = document.createElement("spantext");
+        Deadline.setAttribute('id', 'deadline');
+        
+            
+        var textvalue= input.value;
+        var selopt = select.value;
+        var deadline = inputtime.value;
+        
+        input.value = "" ;
+        switch(selopt){
+          case "red": 
+          iconPriority.setAttribute('src', 'red.png');
+          iconPriority.setAttribute('id', 'indicator');
+          break; 
+          case "yellow": 
+          iconPriority.setAttribute('src', 'yellow.png');
+          iconPriority.setAttribute('id', 'indicator');
+          break; 
+          case "green": 
+          iconPriority.setAttribute('src', 'green.png');
+          iconPriority.setAttribute('id', 'indicator');
+          break; 
+        }
+        spanPriority.append(iconPriority);
+        texttag.append(textvalue);
+        Deadline.append(deadline);
+  
+        iconDelete.setAttribute('src', 'trashicon.png');
+        spanDelete.append(iconDelete);
+  
+        ul.appendChild(li).prepend(spanPriority);
+        ul.appendChild(li).append(texttag, Deadline, spanDelete);
+        
+  
+        deleteTodo();
+        }else{
+          alert("Слишком много задач. Я это все не запомню!")
+        }
+      }
+        
+    });
+}
+
+export function EndTodo(){
+    ul.addEventListener('click', function(ev) {
+          if(ev.target.tagName === 'SPANTEXT'){
+              ev.target.classList.toggle('checked');
+          }
+        
+      },false
+    );
+}
+
+export function CloseShow(){
+    plus_close.addEventListener('click', function(){
+      
+      if(i==1){
+        plus_close.style.transform ="rotate(0deg)";
+        i--;
+      }else
+      {
+        plus_close.style.transform ="rotate(45deg)";
+        i++;
+      }
+      document.getElementById("FullInput").classList.toggle('display');
+    
+    });
+}
+
+export function setToLocalS(){
+    saveBtn.addEventListener('click',function(){
+      localStorage.setItem('todoList',ul.innerHTML );
+      
+    });
+}
+
+export function removefromLocalS(){
+    clearBtn.addEventListener('click', function(){
+      ul.innerHTML= "";
+      localStorage.removeItem('todoList',ul.innerHTML );
+    });
+  }
+
+  export function overlayshow(){
+    tipsBtn.addEventListener("click",function(){
+      overlay.style.height = "100%";
+    });
+  }
+
+  export function overlayhide(){
+    closeBtn.addEventListener("click",function(e){
+      e.preventDefault;
+      overlay.style.height = "0";
+      
+    });
+  }
--- ToDoList (GreatDay) (Final)/script.js
+++ ToDoList (GreatDay) (Final)/script.js
@@ -1,3 +1,5 @@
+import * as event from './event.js';
+
 var input = document.querySelector("input[type = 'text']");
 var inputtime = document.querySelector("input[type = 'time']");
 var select = document.getElementById("priority");
@@ -71,133 +73,29 @@ switch (date.getHours()){
         break;
 }
 
-
-function deleteTodo(){
-  for(let span of spans){
-    span.addEventListener ("click",function (){
-      span.parentElement.remove();
-      event.stopPropagation();
-    });
-  }
-}
+event.deleteTodo();
 
 function loadTodo(){
   if(localStorage.getItem('todoList')){
     ul.innerHTML = localStorage.getItem('todoList');
-    deleteTodo();
+    event.deleteTodo();
   }
 }
 
+event.inputListener();
 
-input.addEventListener("keypress",function(keyPressed){
-  if(keyPressed.which === 13){
-    if(ul.offsetHeight <= (body.offsetHeight-320)){
-    var li = document.createElement("li");
-
-    var texttag = document.createElement("spantext");
-    texttag.setAttribute('id', 'text_container');
-
-    var spanPriority = document.createElement("spanother");
-    spanPriority.setAttribute('id', 'indicator_container');
-    var iconPriority = document.createElement("img");
-
-    var spanDelete = document.createElement("span");
-    spanDelete.setAttribute('id', 'basket');
-    var iconDelete = document.createElement("img");
-
-    var Deadline = document.createElement("spantext");
-    Deadline.setAttribute('id', 'deadline');
-    
-        
-    var textvalue= input.value;
-    var selopt = select.value;
-    var deadline = inputtime.value;
-    
-    input.value = "" ;
-    switch(selopt){
-      case "red": 
-      iconPriority.setAttribute('src', 'red.png');
-      iconPriority.setAttribute('id', 'indicator');
-      break; 
-      case "yellow": 
-      iconPriority.setAttribute('src', 'yellow.png');
-      iconPriority.setAttribute('id', 'indicator');
-      break; 
-      case "green": 
-      iconPriority.setAttribute('src', 'green.png');
-      iconPriority.setAttribute('id', 'indicator');
-      break; 
-    }
-    spanPriority.append(iconPriority);
-    texttag.append(textvalue);
-    Deadline.append(deadline);
-
-    iconDelete.setAttribute('src', 'trashicon.png');
-    spanDelete.append(iconDelete);
-
-    ul.appendChild(li).prepend(spanPriority);
-    ul.appendChild(li).append(texttag, Deadline, spanDelete);
-    
-
-    deleteTodo();
-    }else{
-      alert("Слишком много задач. Я это все не запомню!")
-    }
-  }
-    
-});
-
-
-ul.addEventListener('click', function(ev) {
-      if(ev.target.tagName === 'SPANTEXT'){
-          ev.target.classList.toggle('checked');
-      }
-    
-  },false
-);
-
-
+event.EndTodo();
 
 var i=1;
-plus_close.addEventListener('click', function(){
-  
-  if(i==1){
-    plus_close.style.transform ="rotate(0deg)";
-    i--;
-  }else
-  {
-    plus_close.style.transform ="rotate(45deg)";
-    i++;
-  }
-  document.getElementById("FullInput").classList.toggle('display');
- 
-});
+event.CloseShow();
+
+event.setToLocalS();
+
+event.removefromLocalS();
+
+event.overlayshow();
+
+event.overlayhide();
 
 
-
-
-saveBtn.addEventListener('click',function(){
-  localStorage.setItem('todoList',ul.innerHTML );
-  
-});
-
-
-clearBtn.addEventListener('click', function(){
-  ul.innerHTML= "";
-  localStorage.removeItem('todoList',ul.innerHTML );
-});
-
-
-tipsBtn.addEventListener("click",function(){
-  overlay.style.height = "100%";
-});
-
-
-closeBtn.addEventListener("click",function(e){
-  e.preventDefault;
-  overlay.style.height = "0";
-  
-})
-
-deleteTodo();
 loadTodo();
